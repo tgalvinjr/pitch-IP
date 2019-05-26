2104d9f29f2dfb59676c684fa785fde5cbbd35e9
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-26 20:45:30 +0300

    created views and linked template files

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,85 @@
-from flask import render_template, request, redirect, url_for,abort
+from flask import render_template,url_for,abort,request,redirect
 from . import main
-from .. import db
-
+from app import create_app
+from ..models import User,Pitch,Comment
+from .forms import UpdateProfile, CommentForm
+from flask_login import login_required, current_user
+from .. import db,photos
+import datetime
+# import markdown2
 @main.route('/')
 def index():
-    return '<h1> Hello World </h1>'
+    title = 'Home - Welcome to Pitch App'
+    return render_template('index.html')
+@main.route('/pitch/', methods = ['GET','POST'])
+def pitch():
+
+    title ="PITCH- Share your pitch..."
+
+    pitch = Pitch.get_pitch(id)
+    posted_date = pitch.posted.strftime('%b %d, %Y')
+
+    if request.args.get("like"):
+        pitch.likes = pitch.likes + 1
+
+        db.session.add(pitch)
+        db.session.commit()
+
+        return redirect("/pitch/{pitch_id}".format(pitch_id=pitch.id))
+
+    elif request.args.get("dislike"):
+        pitch.dislikes = pitch.dislikes + 1
+
+        db.session.add(pitch)
+        db.session.commit()
+
+        return redirect("/pitch/{pitch_id}".format(pitch_id=pitch.id))
+
+    comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        comment = comment_form.text.data
+
+        new_comment = Comment(comment = comment,user = current_user,pitch_id = pitch)
+
+        new_comment.save_comment()
+
+
+    comments = Comment.get_comments(pitch)
+
+    return render_template("pitch/pitch.html", pitch = pitch, comment_form = comment_form, comments = comments, date = posted_date)
+  
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
