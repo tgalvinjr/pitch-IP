1fb4a749e433d46bf3d7655e0149c84e725de069
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-26 20:25:15 +0300

    link database to Heroku

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     pass
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     pass
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager,Server
 from app import create_app,db
 from  flask_migrate import Migrate, MigrateCommand
 
-app = create_app('default')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
