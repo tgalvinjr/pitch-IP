329b5c2ce033cf4b13b9e46ea5644920ea27bc73
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-26 23:09:32 +0300

    reinstalled virtual

--- config.py
+++ config.py
@@ -1,26 +1,26 @@
 import os
 
 class Config:
-    SECRET_KEY = 'loise'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://loise:32123772@localhost/pitched'
+    SECRET_KEY = 'alvin'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alvin:alvin123@localhost/pitch'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'loisemwarangu@gmail.com'
-    MAIL_PASSWORD = 'loise1993'
+    MAIL_USERNAME = 'tgalvin.jr@gmail.com'
+    MAIL_PASSWORD = 'alvin123'
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://loise:32123772@localhost/pitched'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alvin:alvin123@localhost/pitch'
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://loise:32123772@localhost/pitched_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alvin:alvin123@localhost/pitch_test'
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://loise:32123772@localhost/pitched'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alvin:alvin123@localhost/pitch'
     DEBUG = True
 
 config_options = {
