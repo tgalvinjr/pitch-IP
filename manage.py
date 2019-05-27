617afb0dad2f2a85bc919275a796679d05e8176a
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-27 12:34:49 +0300

    switch from development to production mode

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from app.models import User,Role
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 manager = Manager(app)
 manager.add_command('server',Server)
 migrate = Migrate(app, db)
